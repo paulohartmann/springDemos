b9ad412f150716b69711a44f40454da411b4c8f8
Paulo Hartmann <paulohar@gmail.com>
2021-02-03 17:07:12 +0100

    some initial codes

---
+++ entityrelationship/src/test/java/com/phlab/entityrelationship/EntityrelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phlab.entityrelationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityrelationshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
